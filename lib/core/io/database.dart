ceec0a56d98624b374f4af0abc153ebba0e6168b
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-09 00:25:35 -0300

    Melhorias na classe de comunicação com api

--- lib/core/io/database.dart
+++ lib/core/io/database.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:pokedex_egsys/core/io/exception.dart';
 import 'package:pokedex_egsys/core/io/idatabase.dart';
@@ -16,30 +16,69 @@ class Database implements DataBaseInterface
   Future<PokemonData> getPokemonByName(String name) async {
     try {
       final request = await http.get(apiBase + "/pokemon/$name");
-      return PokemonData.fromJson(request.body);
+      if(request.statusCode == 200) {
+        return PokemonData.fromJson(request.body);
+      } 
+      else {
+        throw IOException(message: "Can't get pokemon by name $name");
+      }
     } 
     catch (ex) {
-      throw IOException(message: ex);
+      throw IOException(message: "Algo de errado aconteceu!");
     }
   }
   
   @override
-  Future<List<PokemonData>> getPokemonByType(String type) {
-    throw UnimplementedError();
+  Future<List<String>> getPokemonsNameByType(String type) async {
+    try {
+      final request = await http.get(apiBase + "/type/$type");
+
+      if(request.statusCode != 200) {
+        throw IOException(message: "Can't get pokemons by type $type");
+      }
+      
+      Map<String, dynamic> data = jsonDecode(request.body);
+      List<dynamic> results = data['pokemon'];
+    
+      return results.map<String>((e) => e['pokemon']['name']).toList();
+    } 
+    catch (ex) {
+      throw IOException(message: "Algo de errado aconteceu!");
+    }
   }
 
   @override
-  Future<PokemonData> getRandomPokemon() async {
-
-    var rng = Random();
-    int id = rng.nextInt(898);
-
+  Future<PokemonData> getPokemonById(int id) async {
     try {
       http.Response request = await http.get(apiBase + "/pokemon/$id");
-      return PokemonData.fromJson(request.body);
+      if(request.statusCode == 200) {
+        return PokemonData.fromJson(request.body);
+      } 
+      else {
+        throw IOException(message: "Can't get pokemon by id $id");
+      }
     } 
     catch (ex) {
-      throw IOException(message: ex);
+      throw IOException(message: "Algo de errado aconteceu!");
+    }
+  }
+
+  @override
+  Future<List<String>> getPokemonsNameByRange(int offset, int quantity) async {
+    try {
+      http.Response request = await http.get(apiBase + "pokemon?limit=$quantity&offset=$offset");
+
+      if(request.statusCode != 200) {
+        throw IOException(message: "Can't get pokemons by range $offset");
+      }
+
+      Map<String, dynamic> data = jsonDecode(request.body);
+      List<dynamic> results = data['results'];
+
+      return results.map<String>((e) => e['name']).toList();
+    } 
+    catch (ex) {
+      throw IOException(message: "Algo de errado aconteceu@!");
     }
   }
   
--- lib/core/io/idatabase.dart
+++ lib/core/io/idatabase.dart
@@ -3,6 +3,7 @@ import 'package:pokedex_egsys/data/pokemon.dart';
 abstract class DataBaseInterface
 {
   Future<PokemonData> getPokemonByName(String name);
-  Future<List<PokemonData>> getPokemonByType(String type);
-  Future<PokemonData> getRandomPokemon();
+  Future<List<String>> getPokemonsNameByType(String type);
+  Future<PokemonData> getPokemonById(int id);
+  Future<List<String>> getPokemonsNameByRange(int offset, int quantity);
 }
