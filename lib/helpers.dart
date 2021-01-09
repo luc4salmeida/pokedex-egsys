10f0fb13a230e9363dee6fef93e267a2c30e2c03
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-09 00:25:18 -0300

    Adicionado extensão para string caps, cores para tipos de pokémons e estilização.

---
+++ lib/helpers.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Map<String, Color> typeColors = {
+  "normal": Colors.orange,
+  "fightning": Colors.brown,
+  "flying": Colors.teal,
+  "poison": Colors.deepPurple,
+  "ground": Colors.brown,
+  "rock": Colors.grey,
+  "bug": Colors.lime,
+  "ghost": Colors.purple,
+  "steel": Colors.blueGrey,
+  "fire": Colors.red,
+  "water": Colors.blue,
+  "grass": Colors.green,
+  "electric": Colors.yellow,
+  "ice": Colors.cyan,
+  "dragon": Colors.cyan,
+  "dark": Colors.deepPurple,
+  "fairy": Colors.pink
+};
--- lib/style.dart
+++ lib/style.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData brightTheme = ThemeData(
-  primarySwatch: Colors.blue,
+  primarySwatch: Colors.red,
   visualDensity: VisualDensity.adaptivePlatformDensity,
+  textTheme: GoogleFonts.openSansTextTheme()
 );
+
+
+enum MyTypography {
+  SMALL,
+  MEDIUM,
+  BIG
+}
+
+extension Pallet on MyTypography
+{
+  TextStyle get() {
+    switch(this) {
+      case MyTypography.SMALL:  
+        return TextStyle(fontSize: 12.0);
+      case MyTypography.MEDIUM:
+        return TextStyle(fontSize: 16.0);
+       case MyTypography.BIG:
+        return TextStyle(fontSize: 24.0);
+      default:
+        return TextStyle(fontSize: 12.0);
+    }
+  }
+} 
