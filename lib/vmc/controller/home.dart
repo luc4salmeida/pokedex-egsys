44bf683f7b4d8e092ab7abc6f8297b495fbd37c4
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-09 00:26:22 -0300

    Melhorias na tela principal e infinity scrolling adicionado.

--- lib/vmc/controller/home.dart
+++ lib/vmc/controller/home.dart
@@ -1,8 +1,14 @@
+import 'dart:collection';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:pokedex_egsys/core/io/database.dart';
+import 'package:pokedex_egsys/core/io/exception.dart';
+import 'package:pokedex_egsys/data/pokemon.dart';
+import 'package:pokedex_egsys/vmc/controller/pokemon.dart';
+import 'package:pokedex_egsys/widgets/search_pokemon.dart';
+import 'package:toast/toast.dart';
 import '../view/home.dart';
-
-
-import '../model/home.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -14,18 +20,118 @@ class HomeScreen extends StatefulWidget {
 
 class HomeScreenController extends State<HomeScreen> {
 
-  HomeModel model;
+  Database _database;
+  int _offset;
+  int _fetchAmmount;
+
+  bool _searchingForPokemon;
+  bool get isSearchingForPokemon => _searchingForPokemon;
+
+  List<String> _pokemonsName;
+  UnmodifiableListView<String> get pokemonsName =>  UnmodifiableListView(_pokemonsName);
+
+  ScrollController _scrollController;
+  ScrollController get scrollController => _scrollController;
 
   @override
   void initState() {
     super.initState();
-    model = HomeModel();
 
-    _init();
+    _offset = 0;
+    _fetchAmmount = 10;
+
+    _database = Database();
+    _pokemonsName = List();
+
+    _searchingForPokemon = false;
+
+    _scrollController = ScrollController();
+
+    _scrollController.addListener(() {
+      _handleScrollEvents();
+    });
+
+    _fetch();
   }
 
-  _init() async {
-    await model.getRandomPokemon();
+  @override
+  dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  _fetch() async {
+    try {
+      List<String> names = await _database.getPokemonsNameByRange(_offset, _fetchAmmount);
+      _offset += _fetchAmmount;
+
+      setState(() {
+        _pokemonsName.addAll(names);
+      });
+      
+    } 
+    on IOException catch (ex) {
+      _showToast(ex.message);
+    }
+  }
+
+  _handleScrollEvents() {
+    if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      _fetch();
+    }
+  }
+
+  Future<void> onRandomButtonPressed() async {
+
+    Random rng = Random();
+    int id = rng.nextInt(898);
+  
+    try {
+      
+      _changeSearchingForPokemonStatus(true);
+      PokemonData pokemonData = await _database.getPokemonById(id);
+      _changeSearchingForPokemonStatus(false);
+
+      Navigator.push(context, MaterialPageRoute(builder: (context) => PokemonScreen(
+        data: pokemonData
+      )));
+
+    } 
+    on IOException catch(ex) {
+      _showToast(ex.message);
+    }
+    
+  }
+
+  Future<void> onSearchPressed() async {
+
+    FocusScope.of(context).requestFocus(new FocusNode());
+
+    showSearch(
+      context: context, delegate: SearchPokemonDelegate(
+        _database
+      ),
+    );
+  }
+
+  onPokemonPressed(PokemonData data) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => PokemonScreen(
+      data: data
+    )));
+  }
+
+  Future<PokemonData> getPokemonName(String name) async {
+    return _database.getPokemonByName(name);
+  }
+
+  _showToast(String message) {
+    Toast.show(message, context);
+  }
+
+  _changeSearchingForPokemonStatus(bool status) {
+    setState(() {
+      _searchingForPokemon = status;
+    });
   }
 
   @override
--- lib/vmc/view/home.dart
+++ lib/vmc/view/home.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
+import 'package:pokedex_egsys/style.dart';
+import 'package:pokedex_egsys/widgets/pokemon_list_tile.dart';
+
 import 'package:flutter/material.dart';
+
 import 'package:pokedex_egsys/data/pokemon.dart';
 import 'package:pokedex_egsys/widgets/base_view.dart';
 
@@ -10,35 +16,118 @@ class HomeScreenView extends WidgetView<HomeScreen, HomeScreenController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _buildPokemonStream(context))
-    );
-  }
-  
-  Widget _buildPokemonStream(BuildContext context) {
-    return StreamBuilder(
-      stream: state.model.randomPokemon,
-      builder: (context, snap) {
-        if(snap.hasError) return _buildPokemonError(context);
-        else if(!snap.hasData) return _buildPokemonLoading(context);
-        else return _buildPokemon(context, snap.data);
-      }
+      body: _buildBody(context),
+      appBar: AppBar(
+        title: Text("Egsys Pokedex"),
+        centerTitle: true,
+      ),
     );
   }
 
-  Widget _buildPokemonLoading(BuildContext context) {
-    return CircularProgressIndicator();
-  }
-
-  Widget _buildPokemonError(BuildContext context) {
-    return Text("Algo de errado aconteceu!");
-  }
-
-  Widget _buildPokemon(BuildContext context, PokemonData data) {
-    return Column(
+  Widget _buildBody(BuildContext context) {
+    return Stack(
       children: [
-        Text(data.name),
-        Image.network(data.imageUrl)
+        AbsorbPointer(
+          absorbing: state.isSearchingForPokemon,
+          child: CustomScrollView(
+            controller: state.scrollController,
+            slivers: [
+              _buildSearchBar(context),
+              _buildRandomButtonPokemon(context),
+              _buildExploreTitle(context),
+              _buildPokemonList(context)
+            ],
+          ),
+        ),
+        state.isSearchingForPokemon ? Positioned.fill(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 5,
+              sigmaY: 5
+            ),
+            child: Container(
+              color: Colors.black.withOpacity(0)
+            )
+          )
+        ) : SizedBox(),
+        state.isSearchingForPokemon ? _buildSearchingForPokemon(context) : SizedBox(),
       ],
+    );
+  }
+
+  Widget _buildSearchBar(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Digite aqui o nome ou tipo do pokémon!"
+          ),
+          readOnly: true,
+          onTap: state.onSearchPressed,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRandomButtonPokemon(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: RaisedButton.icon(
+        onPressed: state.onRandomButtonPressed, 
+        icon: Icon(Icons.explore), 
+        label: Text("Descubra um Pokémon!"),
+        color: Colors.redAccent,
+        textColor: Colors.white,
+      ),
+    );
+  }
+
+  Widget _buildExploreTitle(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text("Explore", style: MyTypography.BIG.get().copyWith(color: Colors.grey[300])),
+      ),
+    );
+  }
+
+  Widget _buildPokemonList(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => index < state.pokemonsName.length ? FutureBuilder<PokemonData>(
+          future: state.getPokemonName(state.pokemonsName[index]),
+          builder: (context, snap) {
+            if(snap.hasData) return GestureDetector(
+              onTap: () => state.onPokemonPressed(snap.data),
+              child: PokemonListTile(data: snap.data)
+            );
+            return PokemonLoadingListTile();
+          },
+        ) : _buildLoadingBottom(context),
+        childCount: state.pokemonsName.length + 1,
+      )
+    );
+  }
+
+  Widget _buildLoadingBottom(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: CircularProgressIndicator(),
+    ));
+  }
+
+  Widget _buildSearchingForPokemon(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CircularProgressIndicator(),
+          SizedBox(height: 10.0),
+          Text("Procurando um pokémon legal pra você ;)", style: MyTypography.MEDIUM.get().copyWith(color: Colors.black54))
+        ],
+      ),
     );
   }
 }
